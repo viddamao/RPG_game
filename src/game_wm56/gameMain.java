448aef1a3552fd44b674f852c75ddcdba39bd19f
Wenjun Mao <wm56@duke.edu>
2014-08-31 23:08:50 -0400

    main game class

--- src/game_wm56/gameMain.java
+++ src/game_wm56/gameMain.java
@@ -1,11 +1,14 @@
 package game_wm56;
 
 import java.io.File;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -15,16 +18,32 @@ public class gameMain extends Application {
 
 public void start(Stage stage){
 	 ImageView background=new ImageView();
-	 File file = new File(".//res//background//map.jpg");
-	 Image img = new Image(file.toURI().toString());
-	 background.setImage(img);
+	 File file = new File(".//res//background//grid.jpg");
+	 Image map = new Image(file.toURI().toString());
+	 
+	 background.setImage(map);
 	 Group root = new Group();
 	 Scene scene = new Scene(root);
 	 scene.setFill(Color.BLACK);
-	 HBox box = new HBox();
+	 HBox bgbox = new HBox();
+     bgbox.getChildren().add(background);
 	 
-	 box.getChildren().add(background);
-	 root.getChildren().add(box);
+     file = new File(".//res//hero//paladin.jpg");
+     Image heroImage=new Image(file.toURI().toString(),80,80,true,false);
+	 
+	 ImageView hero=new ImageView();
+	 hero.setImage(heroImage);
+	 
+	  AnchorPane anchorpane = new AnchorPane();
+	 
+	  
+	  
+	 HBox box2=new HBox();
+	 box2.getChildren().add(hero);
+	 box2.relocate(110, 210);
+	 root.getChildren().add(bgbox);
+	 root.getChildren().add(box2);
+	 
 	 stage.setTitle("Legend of Paladins");
 	 stage.setResizable(false);
 	 stage.setScene(scene); 
